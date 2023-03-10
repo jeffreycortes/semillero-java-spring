a223f09c7517f3d227a4ca982dee80570493a176
Jeffrey Alberto Cortés Cuesta <jeffrey.cortes@externos.bci.cl>
2023-03-09 19:29:56 -0500

    Se actualiza condición del user validation

--- src/main/java/com/semillerogtc/gtcusermanagament/components/UsersValidation.java
+++ src/main/java/com/semillerogtc/gtcusermanagament/components/UsersValidation.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class UsersValidation {
     public boolean execute(String user) {
-        return  (user == "Jeff");
+        return  user.equals("Jeff");
     }
 }
