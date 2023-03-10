81301dd879f8e02bebe99b2e989218c1e04687d4
Jeffrey Alberto Cortés Cuesta <jeffrey.cortes@externos.bci.cl>
2023-03-09 19:24:20 -0500

    Se suben dtos

--- src/main/java/com/semillerogtc/gtcusermanagament/domain/UsuarioDto2.java
+++ src/main/java/com/semillerogtc/gtcusermanagament/domain/UsuarioDto2.java
@@ -2,9 +2,11 @@ package com.semillerogtc.gtcusermanagament.domain;
 
 import javax.validation.constraints.NotEmpty;
 
-public class UsuarioDto {
+public class UsuarioDto2 {
     @NotEmpty
     public String email;
     @NotEmpty
     public String userId;
+    @NotEmpty
+    public String celular;
 }
