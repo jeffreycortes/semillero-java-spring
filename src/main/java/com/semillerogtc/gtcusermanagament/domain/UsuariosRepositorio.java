e8b4f813ca7d10352b5aab006819ab85f8ca9fb3
Jeffrey Alberto Cortés Cuesta <jeffrey.cortes@externos.bci.cl>
2023-03-10 20:02:48 -0500

    Se configur jpa y h2 y se implementa usuario repository

---
+++ src/main/java/com/semillerogtc/gtcusermanagament/domain/UsuariosRepositorio.java
@@ -0,0 +1,8 @@
+package com.semillerogtc.gtcusermanagament.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuariosRepositorio extends JpaRepository<Usuario, String> {
+}
