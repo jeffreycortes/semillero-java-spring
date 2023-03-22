4847eb5e39fd40b7b5fbd5ea8a0c6a2d4a715299
Jeffrey Alberto Cortés Cuesta <jeffrey.cortes@externos.bci.cl>
2023-03-22 11:30:40 -0500

    Se configura DevEnvironment como profile por defecto

--- src/main/java/com/semillerogtc/gtcusermanagament/infrastructure/environment/DevEnvironmentService.java
+++ src/main/java/com/semillerogtc/gtcusermanagament/infrastructure/environment/DevEnvironmentService.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-@Profile("dev")
+@Profile({"dev", "default"})
 public class DevEnvironmentService implements EnvironmentService {
     @Override
     public String getEnvironmentName() {
